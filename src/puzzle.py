a613e8671cb4e5eef51c036a0de99cbce7539308
Max Tremaine <me@maxtremaine.com>
2021-02-15 00:57:29 -0500

    Setup functions for navigating puzzles.

--- src/puzzle.py
+++ src/puzzle.py
@@ -11,3 +11,34 @@ def validate_sudoku_string(sudoku_string):
         return False
     return True
 
+def validate_cell_index(cell_index):
+    """If an input is less than 81, return True."""
+    try:
+        if cell_index > 80:
+            return False
+    except:
+        return False
+    return True
+
+def get_related_cells(cell_index):
+    """Returns a list of related cells."""
+    related_groups = [
+        n for n in puzzle_rules['groups']
+        if cell_index in n
+    ]
+    related_cells = [
+        y for x in related_groups for y in x
+    ]
+    unique_related_cells = {
+        x for x in related_cells
+    }
+    return list(unique_related_cells)
+
+def get_cell_values(cell_indexes):
+    def _(sudoku_string):
+        return [
+            sudoku_string[cell_index] for cell_index in cell_indexes
+        ]
+    return _
+
+print(get_cell_values([1, 2, 3])('0174'))
