3e4ee0e98404dcf4b58f0fd75bb04c5ae65049e0
Max Tremaine <max.tremaine@gmail.com>
2021-03-13 12:06:45 -0500

    Added prioritization logic.

--- __main__.py
+++ __main__.py
@@ -7,6 +7,21 @@ with open('io/start_puzzle.sudoku', 'r') as f:
     start_puzzle = f.read()
 
 puzzle_string = sudoku_file_to_string(start_puzzle)
-degs = cell_degrees_of_freedom(4)(puzzle_string)
 
-print(degs)
+underscores = [
+    {
+        'index': i,
+        'value': x,
+        'degrees_of_freedom': cell_degrees_of_freedom(i)(puzzle_string)
+    }
+    for i, x in enumerate(puzzle_string)
+    if x == '_'
+]
+
+prioritized_underscores = sorted(
+    underscores,
+    key = lambda underscore: underscore['degrees_of_freedom'],
+    reverse = True
+)
+
+print(prioritized_underscores)
